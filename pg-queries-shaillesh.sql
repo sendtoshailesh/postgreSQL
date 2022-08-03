b6f9d9fdf4ef5b73517f0826a372d3b2e89d3391
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-08-03 18:30:35 +0530

    Create pg-queries-shaillesh.sql

---
+++ pg-queries-shaillesh.sql
@@ -0,0 +1,18 @@
+View connection information
+
+SELECT datname,usename,client_addr,client_port FROM pg_stat_activity ;
+
+
+Query the SQL execution information
+You can execute the following SQL statements to view current queries executed by users who connect to the database:
+
+
+
+
+bench=# \x
+bench=# SELECT query, calls, total_exec_time, rows, 100.0 * shared_blks_hit /
+               nullif(shared_blks_hit + shared_blks_read, 0) AS hit_percent
+          FROM pg_stat_statements ORDER BY total_exec_time DESC LIMIT 5;
+          
+          
+          
